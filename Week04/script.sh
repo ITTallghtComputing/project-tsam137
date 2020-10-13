f18ab5ae0e73292dc1dfdab192015f493bd4977b
tsam137 <tobasamuel137@gmail.com>
2020-10-13 12:04:04 +0000

    non virtualization of hardware in containers

---
+++ Week04/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello World"
+
+echo "There are $cnt processors on this instance"
---
+++ Week3/first.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# store the number of process
+# into variable cnt
+cnt=`ps -ef | wc -l`
+
+#
+# bash integer equality operator
+#
+if [ "$cnt" -gt "$1" ];
+
+then
+  echo "number of processes exceeded"
+fi
