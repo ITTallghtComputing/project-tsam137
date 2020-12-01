2cca15e9244b259488c7445b39fa2f7a5ca10015
tsam137 <tobasamuel137@gmail.com>
2020-12-01 13:09:29 +0000

    Pubsub Lab 2

---
+++ EPA/pubsub/recv_m2n.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+###########################################
+# the number of messages to send
+###########################################
+message_count=50
+ 
+###########################################
+# the topic to send the messages to
+###########################################
+#topic_name=newsfeed-topic
+ 
+###########################################
+# for loop in bash
+###########################################
+for (( c=0; c<$message_count; c++ ))
+do
+        gcloud pubsub subscriptions pull PROD-subs-A --auto-ack
+        gcloud pubsub subscriptions pull PROD-subs-B --auto-ack
+done
+
