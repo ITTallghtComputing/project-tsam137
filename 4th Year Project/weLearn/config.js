2a7ebac49631d01af44cd4b31e16ce9912d5fb99
tsam137 <tobasamuel137@gmail.com>
2021-03-13 20:52:32 +0000

    Update config.js

--- 4th Year Project/weLearn/config.js
+++ 4th Year Project/weLearn/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoUri: "mongodb+srv://admin:welearn127@welearn.avb3e.mongodb.net/welearndb?retryWrites=true&w=majority",
+    mongoUri: process.env.MONGO_URI ,
     PORT: process.env.PORT || 3000,
 }
